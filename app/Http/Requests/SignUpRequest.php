de8c99d2efe473ed5961e046c4b53579c94b9c0c
kimsa-0605 <holykimsa05@gmail.com>
2025-03-25 23:21:03 +0700

    Kimsa 2 (#7)
    * 2 | create controller to handle sign up feature - kimsa
    
    * 2 | create controller to handle sign up feature - kimsa

--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -1,24 +1,25 @@
 <?php
-
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterRequest extends FormRequest
+class SignUpRequest extends FormRequest
 {
     public function authorize(): bool
     {
         return true;
     }
+
     public function rules(): array
     {
         return [
-                'email' => 'required|email|max:255|unique:users,email',
-                'password' => 'required|min:6|max:50',
-                'fullname' => 'required|string|min:3|max:50',
-                'confirmPassword'=> 'required|min:6|confirmed',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:6|max:50',
+            'fullname' => 'required|string|min:3|max:50',
+            'confirmPassword' => 'required|min:6|same:password',
         ];
     }
+
     public function messages()
     {
         return [
@@ -28,7 +29,8 @@ class RegisterRequest extends FormRequest
             'password.required' => 'Password cannot be empty.',
             'password.min' => 'Password must be at least 6 characters.',
             'fullname.min' => 'Full name must be at least 3 characters.',
-            'confirmPassword' => 'Confirm password does not match',
+            'confirmPassword.required' => 'Confirm password cannot be empty.',
+            'confirmPassword.same' => 'Confirm password does not match the password.',
         ];
     }
 }
