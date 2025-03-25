26d995a82fe997ef85ecd9f4e5950cc24ec2601b
nguyendat030805 <dat656473@gmail.com>
2025-03-25 09:22:57 +0700

    2 | create send email | dat

--- resources/views/emails.blade.php
+++ resources/views/emails.blade.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng ký thành công</title>
+    <title>Registration successful</title>
 </head>
 <body>
     <h2>Chào {{ $user['fullname'] }},</h2>
-    <p>Cảm ơn bạn đã đăng ký tại <strong>Your App Name</strong>!</p>
-    <p>Thông tin đăng ký:</p>
+    <p>Thank you for register account <strong>ToyStore</strong>!</p>
+    <p>Register Information</p>
     <ul>
-        <li><strong>Họ và tên:</strong> {{ $user['fullname'] }}</li>
+        <li><strong>Full name</strong> {{ $user['fullname'] }}</li>
         <li><strong>Email:</strong> {{ $user['email'] }}</li>
-        <li><strong>Số điện thoại:</strong> {{ $user['phone'] }}</li>
     </ul>
-    <p>Chúc bạn một ngày vui vẻ!</p>
+    <p>Have a good day!</p>
 </body>
 </html>
