fa43d5aca3f05a252674394c565bdd2c9379638e
hothitiep02 <htt301202@gmail.com>
2025-03-26 20:44:36 +0700

    8 | create login request - Tiep

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:6|max:50',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email cannot be empty.',
+            'email.email' => 'Invalid email format.',
+        ];
+    }
+}
