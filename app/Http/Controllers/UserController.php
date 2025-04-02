91d780941f304e27e40c93f899227d5d36b10611
nguyendat030805 <dat656473@gmail.com>
2025-04-02 23:53:34 +0700

    Dat 22 (#31)
    * 13 display products (#27)
    
    * 13 | create migration categories and products and create seeder categories and products | dat
    
    * 13 | create product and category models - kimsa
    
    * Tiep 13 (#24)
    
    * 13 | Create ProductController to render the products in home page - Tiep
    
    * 13  | Create ProductController to render the products in home page - tiep
    
    * 13 | Fix route for homepage-tieptiep
    
    * 13 | fix controller to handle and render data into home page - kimsa
    
    * 13 | update new data in ProductsSeeder - kimsa
    
    ---------
    
    Co-authored-by: kimsa-0605 <holykimsa05@gmail.com>
    
    * Product api kimsa 13 (#26)
    
    * Create ProductController
    
    * 8 | Create ProductController in api folder to render the product in the Catalog page by categories - chang
    
    * 13 | fix api and render data into catalog page - kimsa
    
    ---------
    
    Co-authored-by: ChangSeven2k5 <changcoor7@gmail.com>
    
    ---------
    
    Co-authored-by: nguyendat030805 <dat656473@gmail.com>
    Co-authored-by: hothitiep02 <htt301202@gmail.com>
    Co-authored-by: ChangSeven2k5 <changcoor7@gmail.com>
    
    * 22 | create fogot password | dat
    
    * 22 | create view mail fogot | dat
    
    * 22 | create otp emailServices | dat
    
    ---------
    
    Co-authored-by: kimsa-0605 <holykimsa05@gmail.com>
    Co-authored-by: hothitiep02 <htt301202@gmail.com>
    Co-authored-by: ChangSeven2k5 <changcoor7@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use App\Services\EmailServices\SignUpMail;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
